4fd6be5eedc1518ca2255fb366039882cb4d712f
2Aphish <isabcorrado@gmail.com>
2019-02-17 15:58:13 -0800

    Formatting and documentation
    Minor changes to formatting and added comments for documentation purposes.

--- PizzaGame/pizza_game.py
+++ PizzaGame/pizza_game.py
@@ -1,22 +1,28 @@
 import random
 from time import sleep
 
-# toppings, when randomizing  pepperoni = 0, mushrooms = 1, olives = 2, peppers = 3
+# toppings
 pepperoni = 0
 mushrooms = 0
 olives = 0
 peppers = 0
+
+# current toppings on pizza
 pizza = [pepperoni, mushrooms, olives, peppers]
 
 
 def new_game():
+	# reset pizza toppings
 	pepperoni = 0
 	mushrooms = 0
 	olives = 0
 	peppers = 0
 
+	
 def topping_type():
+	# for randomization:  pepperoni = 0, mushrooms = 1, olives = 2, peppers = 3
 	return random.randint(0,3)
+
 
 def topping_action():
 	# if even toppings will be added, if odd toppings will be removed 
@@ -28,27 +34,19 @@ def topping_action():
 		action = -1
 	return action
 
+
 def add_toppings(action, number, topping):
 	pizza[topping] = pizza[topping] + (action * number)
 
 
 def make_pizza():
 	for turn in xrange(0, 19):
+		
 		# get all things needed during turn
-		action = topping_action() # add/remove
+		action = topping_action()    # add/remove
 		number = random.randint(1,3) # number of that toppings added to pizza 
-		topping = topping_type() # type of topping added to pizza
-
-		# check that there are enough toppings of that type on pizza to remove
-		if (action < 1) and (pizza[topping] < number):
-			action = 1
-			
-		# check to make sure there are not too many toppings aleady on pizza or if adding the toppings will exceed max (6)
-		if pizza[topping] >= 6 or (pizza[topping]+number) >=6:
-			action = -1
-
-		add_toppings(action, number, topping)
-
+		topping = topping_type()     # type of topping added to pizza
+		
 		# get topping type as a string
 		if topping == 0:
 			topping_str = "PEPPERONI"
@@ -60,6 +58,16 @@ def make_pizza():
 			topping_str = "PEPPERS"
 		else:
 			topping_str = "ERROR: Topping type."
+		
+		# if toppings are to be removed, check that there are enough toppings of that type on pizza
+		if (action < 1) and (pizza[topping] < number):
+			action = 1
+			
+		# each topping can have a max of 6 on pizza at a time, check to make sure adding the toppings will exceed max
+		if pizza[topping] >= 6 or (pizza[topping]+number) >=6:
+			action = -1
+
+		add_toppings(action, number, topping)
 
 		# print instructions
 		if action > 0:
@@ -78,7 +86,9 @@ def playgame():
 	sleep(0.5)
 	print "Here we go."
 	sleep(1)
+	
 	make_pizza()
+	
 	print "Great job!"
 	sleep(1)
 	print "You should have", pizza[0], "pepperoni,", pizza[1], "mushrooms,", pizza[2], "olives and", pizza[3], "peppers."
@@ -89,6 +99,7 @@ def playgame():
 def main():
 	playgame()
 	sleep(0.5)
+	
 	while True:
 		#print "Would you like to play again? (Y/N)"
 		s = raw_input("Would you like to play again? (Y/N)").upper()
